1bf9c8e6f8932053e68a249c4bf4e4d33726297b
victorbaamonde <baamonde.victor@gmail.com>
2023-08-26 19:57:45 -0300

    comitando de novo

--- exemplosFuncoes.py
+++ exemplosFuncoes.py
@@ -1,7 +1,7 @@
 # ---------  EXAMPLE 1  --------- 
 def display_invoice(username, amount, due_date):
-   print(f"Hello {username}")
-   print(f"Your bill of ${amount:.2f} is due: {due_date}")
+    print(f"Hello {username}")
+    print(f"Your bill of ${amount:.2f} is due: {due_date}")
 
 # display_invoice("BroCode", 42.50, "01/02")
 # display_invoice("JoeSchmo", 100.99, "02/03")
