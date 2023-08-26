f335b8c08fa49a821ce70f1a84020983b0c9123a
victorbaamonde <baamonde.victor@gmail.com>
2023-08-26 19:57:06 -0300

    comitando de novo

---
+++ exemplosFuncoes.py
@@ -0,0 +1,16 @@
+# ---------  EXAMPLE 1  --------- 
+def display_invoice(username, amount, due_date):
+   print(f"Hello {username}")
+   print(f"Your bill of ${amount:.2f} is due: {due_date}")
+
+# display_invoice("BroCode", 42.50, "01/02")
+# display_invoice("JoeSchmo", 100.99, "02/03")
+
+# ---------  EXAMPLE 2  --------- 
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name("spongebob", "squarepants")
+print(full_name)
